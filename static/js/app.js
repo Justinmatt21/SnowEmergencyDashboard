e13427cc2547fc7ab736397647cf98aa59e8a70e
Justin Douglas <justinmatt21@hotmail.com>
2019-07-05 06:34:24 -0500

    Justin bubble chart app.js additions

--- static/js/app.js
+++ static/js/app.js
@@ -27,12 +27,95 @@ function init() {
     });
   }
 
-function initChart() {
-  d3.json("/sql").then((response) => {
+function initBubble() {
+  url = `/sqlBubble`;
+
+  var bubbleSelector = d3.select("#bubble");
+
+  d3.json(url).then((response) => {
     console.log(response);
+
+    var years = response.year;
+    var tickets = response.tickets;
+    var tows = response.tows;
+    var revenue = response.towing_revenue;
+    var expenses = response["towing expenses"];
+    var revenue_less_expenses = response["towing revenue less expenses"];
+    
+    colors = [];
+    
+    revenue_less_expenses.forEach(function(item) {
+
+      if (item < 0) {
+        colors.push("red");
+      }
+
+      else {
+        colors.push("green");
+      }
+    });
+
+    console.log(colors);
+
+    var trace1 = {
+      x: years,
+      y: tows,
+      text: revenue_less_expenses,
+      hovertemplate:  '<b>Tows:</b> %{y}  <br>'+
+                      '<b>Towing Revenue:</b> %{marker.size:,}  <br>'+
+                      '<b>Towing Revenue minus Expenses:</b> %{text}' +
+                      '<extra></extra>',
+      mode: 'lines+markers',
+      marker: {
+        size: revenue,
+        sizemode: 'area',
+        sizeref: 2000,
+        sizemin: 4,
+        color: colors
+      },
+      name: "tows"
+    };
+
+    var trace2 = {
+      x: years,
+      y: tickets,
+      text: revenue_less_expenses,
+      hovertemplate:  '<b>Tickets:</b> %{y}  <br>'+
+                      '<extra></extra>',
+      mode: 'lines+markers',
+      marker: {
+        color: "black", 
+        symbol: "star",
+        size: 10
+      },
+      name: "tickets"
+    };
+
+    var data = [trace1, trace2];
+
+
+    var layout = {
+      title: {
+        text: "Tows and Tickets per Snow Emergency over Time"
+      },
+      xaxis: {
+        title: "Year",
+        tickmode: "array",
+        tickvals: years,
+        ticktext: years
+      },
+      yaxis: {
+        title: "Number of Tows or Tickets"
+      }
+    }
+
+    Plotly.newPlot("bubble", data, layout);
+
+
   });
 };
 
 // Initialize the dashboard
 init();
+initBubble();
 
